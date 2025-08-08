751d9cad245b6a9b2f4df37053f60c540e7513da
Uditya-Raj <146313034+Uditya-Raj@users.noreply.github.com>
2025-08-08 22:42:02 +0530

    initial commit

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,20 @@
+
+
+import express from "express";
+const app = express();
+
+app.get("/signup",(req,res)=>{
+
+})
+
+app.get("/signin",(req,res)=>{
+
+})
+
+app.get("/chat",(req,res)=>{
+
+})
+
+app.listen(3008,()=>{
+    console.log("listening on port 3000");
+});
