9eecb2406a65fcac340afd811e40cbb73fb858a9
Uditya-Raj <146313034+Uditya-Raj@users.noreply.github.com>
2025-08-10 16:54:05 +0530

    db package added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,12 +2,20 @@ import dotenv from "dotenv";
 import jwt  from "jsonwebtoken";
 import express from "express";
 import { middleware } from "./middleware";
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from  "@repo/common/types";
+import {prismaClient} from "@repo/db/client"
 const app = express();
 dotenv.config();
 const jwtSecret = process.env.JWT_SECRET;
 
 app.get("/signup", (req, res) => {
   // zod validation
+  const data  = CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+     return res.json({
+        message:"Incorrect inputs"
+     })
+  }
    //db call
    res.json({
      userId : "123"
@@ -15,6 +23,13 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/signin", (req, res) => {
+
+      const data = SigninSchema.safeParse(req.body);
+      if (!data.success) {
+        return res.json({
+          message: "Incorrect inputs",
+        });
+      }
      const userId = 1;
      if(!jwtSecret){
         throw new Error("JWT_SECRET environment variable is required");
@@ -29,6 +44,13 @@ app.get("/signin", (req, res) => {
 });
 
 app.get("/room",middleware ,(req, res) => {
+
+     const data = CreateRoomSchema  .safeParse(req.body);
+     if (!data.success) {
+       return res.json({
+         message: "Incorrect inputs",
+       });
+     }
     // db call
 
     res.json({
